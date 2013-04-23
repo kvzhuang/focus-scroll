b0fb88705373e21cd87c2c0a29ca46cebe7a3a7e
kvzhuang <kvzhuang@gmail.com>
2013-04-23 13:58:08 +0800

    Add callback and offset.

--- focus-scroll.js
+++ focus-scroll.js
@@ -20,19 +20,20 @@ YUI.add("focus-scroll", function (Y) {
     FocusScrollPlugin.prototype = {
         _init: function (config) {
             Y.log("_init() is executed.", "info", MODULE_ID);
-            var node = config.host,
+            var callback = config.host.callback || null,
+                node = config.host,
                 focusable = "a";
-            Y.delegate("focus", this._handleFocus, node, focusable, node);
-
+            Y.delegate("focus", this._handleFocus, node, focusable, node, callback);
             // Focus the first element.
             node.one(focusable).focus();
         },
-        _handleFocus: function (e) {
+        _handleFocus: function (e, callback) {
             Y.log("_handleFocus() is executed.", "info", MODULE_ID);
             var anim,
                 container,
                 isDoc,
                 node,
+                offset = 100, // offset to scroll
                 nodeY,        // node offsetY position.
                 nodeHeight,   // Height of each node. (MUST BE THE SAME)
                 rowTotal,     // Available amount of rows within the viewport.
@@ -40,18 +41,21 @@ YUI.add("focus-scroll", function (Y) {
                 scrollHeight, // Current scroll height.
                 scrollTop,    // Target scrollTop value.
                 viewHeight;
-
             node = e.currentTarget;
             container = this;
             isDoc = (container._node === document || container._node === window || container._node === document.body);
             nodeY = (isDoc) ? node.get("region").top : node.get("offsetTop") - container.get("region").top;
             nodeHeight = node.get("offsetHeight");
             scrollY = (isDoc) ? node.get("docScrollY") : container.get("scrollTop");
+            Y.log("scrollY = " + scrollY);
             viewHeight = (isDoc) ? node.get("winHeight") : container.get("offsetHeight");
             scrollHeight = viewHeight + scrollY;
+            Y.log("_handleFocus() - Current position " +
+                 "(nodeBottomY = " + (nodeY + nodeHeight) + ", " +
+                 "scrollHeight = " + scrollHeight + ").");
 
             // Scroll down when focused node exceeds viewport.
-            if (nodeY + nodeHeight >= scrollHeight) {
+            if (nodeY + nodeHeight >= scrollHeight - offset) {
                 Y.log("_handleFocus() - Scroll down " +
                      "(nodeBottomY = " + (nodeY + nodeHeight) + ", " +
                      "scrollHeight = " + scrollHeight + ").");
@@ -59,7 +63,7 @@ YUI.add("focus-scroll", function (Y) {
                 scrollTop = nodeY;
 
             // Scroll up.
-            } else if (nodeY < scrollY) {
+            } else if (nodeY < scrollY + offset) {
                 Y.log("_handleFocus() - Scroll up " +
                      "(nodeTopY = " + nodeY + ", " +
                      "scrollY = " + scrollY + ").");
@@ -82,13 +86,21 @@ YUI.add("focus-scroll", function (Y) {
                 });
                 anim.run();
             }
+            if (callback) {
+                callback.apply(e.currentTarget);
+            }
         }
     };
 
     Y.FocusScrollPlugin = FocusScrollPlugin;
-},
-    "0.0.1", {
-        "group"    : "mui",
-        "js"    : "focus-scroll/focus-scroll.js",
-        "requires": ["anim", "event-resize", "event-focus", "event-delegate", "node"]
-    });
+}, "0.0.1", {
+    "group"    : "mui",
+    "js"    : "focus-scroll/focus-scroll.js",
+    "requires": [
+        "anim",
+        "event-resize",
+        "event-focus",
+        "event-delegate",
+        "node"]
+});
+
